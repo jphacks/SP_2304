69e5bd339b89a124818a4125125d9ec2b3ca4267
Reso <84968076+kake-r@users.noreply.github.com>
2023-10-29 02:13:37 +0900

    /api/goolabs/morph done

--- backend/app/api/goolab/views.py
+++ backend/app/api/goolab/views.py
@@ -10,10 +10,30 @@ router = APIRouter(prefix="/goolabs")
 class KeywordItem(BaseModel):
     title: str
     body: str
+
+class MorphItem(BaseModel):
+    sentence: str
+
 @router.post("/morph")
 async def get_keywords(item: MorphItem):
-    pass
-
+    api_key =  os.getenv("GOOLAB_API_KEY")
+    url = f"{BASE_URL}/morph"
+    print(url)
+    payload = {
+        "app_id": api_key,
+        "sentence": item.sentence,
+    }
+    response = requests.post(url, json=payload)
+    word_list = []
+    for word in response.json()["word_list"]:
+        for i in word:
+            if i[1] == "名詞":
+                word_list.append(i[0])
+            
+    print(word_list)
+    word_list += ["", "", ""]
+    return {"word_0": word_list[0], "word_1": word_list[1], "word_2": word_list[2]}
+    
 @router.post("/keywords")
 async def get_keywords(item: KeywordItem):
     api_key =  os.getenv("GOOLAB_API_KEY")
@@ -30,4 +50,4 @@ async def get_keywords(item: KeywordItem):
         word_list += list(i.keys())
     word_list += ["", "", ""]
     return {"word_0": word_list[0], "word_1": word_list[1], "word_2": word_list[2]}
-
+    
