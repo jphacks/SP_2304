53a88caf4918a5631fdb7a4f0301bda7a9cada19
akTARDIGRADE13 <itmtha@gmail.com>
2023-10-29 10:35:50 +0900

    clean files

--- frontend/src/components/FirebaseConfig.tsx
+++ frontend/src/components/FirebaseConfig.tsx
@@ -1,11 +1,11 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDDMJeRLZobW7M1ZpXJ1aTnUHyu-yFd6rM",
+  appId: "1:154053445032:web:6bcab5a382ecf67fd3899d",
   authDomain: "ramenzaifu.firebaseapp.com",
+  measurementId: "G-D13XN5V4KL",
+  messagingSenderId: "154053445032",
   projectId: "ramenzaifu",
   storageBucket: "ramenzaifu.appspot.com",
-  messagingSenderId: "154053445032",
-  appId: "1:154053445032:web:6bcab5a382ecf67fd3899d",
-  measurementId: "G-D13XN5V4KL"
 };
 
-export default firebaseConfig
+export default firebaseConfig;
